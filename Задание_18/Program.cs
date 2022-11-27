79242b69ebd53227d9181d47961d97300a271bd6
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-28 00:20:30 +0500

    Добавьте файлы проекта.

---
+++ Задание_18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_18
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string str = Console.ReadLine();
+        if (Check(str))
+            Console.WriteLine("Строка корректна.");
+        else
+            Console.WriteLine("Строка некорректна");
+
+        Console.ReadLine();
+    }
+
+    static bool Check(string str)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        Dictionary<char, char> dict = new Dictionary<char, char>()
+            {
+                {'(',')' },
+                {'[',']' },
+                {'{','}' }
+            };
+
+        foreach (char c in str)
+        {
+            if (dict.ContainsKey(c))
+                stack.Push(dict[c]);
+            if (dict.ContainsValue(c))
+            {
+                if (stack.Count == 0 || stack.Pop() != c)
+                {
+                    return false;
+                }
+            }
+        }
+        if (stack.Count == 0)
+            return true;
+        return false;
+    }
+}
+}
+
